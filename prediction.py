56bfe295a5c86983a954caee85edead1436aea10
recep <recep.alkaya@naspers.com>
2019-10-18 15:04:33 +0200

    Working Network with h5 data loader & BCE Loss

--- prediction.py
+++ prediction.py
@@ -4,9 +4,14 @@ import torch.nn.functional as F
 import torch.optim as optim
 from torch.utils import data
 
-import pandas as pd
-import numpy as np
+import os
+import sys
 import datetime
+import h5py
+
+this_dir = os.path.abspath(os.path.dirname(__file__))
+projects_dir = os.path.dirname(this_dir)
+if projects_dir not in sys.path: sys.path.append(projects_dir)
 
 
 class Net(nn.Module):
@@ -21,50 +26,42 @@ class Net(nn.Module):
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
         x = F.relu(self.fc3(x))
-        x = torch.sigmoid(self.fc4(x))
-        return x
+        return self.fc4(x)
 
 
-class MyDataset(data.Dataset):
-    def __init__(self, csv_path, chunkSize):
-        self.chunksize = chunkSize
-        self.reader = pd.read_csv(csv_path, sep='|', chunksize=self.chunksize,
-                                  header=None, iterator=True)
-
-    def __len__(self):
-        return self.chunksize
+class DatasetH5(data.Dataset):
+    def __init__(self, in_file):
+        self.file = h5py.File(in_file, 'r')
+        self.data = self.file['data']['table']
 
     def __getitem__(self, index):
-        data = self.reader.get_chunk(self.chunksize)
-        input = data.iloc[:, 1:-1]
-        input = np.array(input)
-        input = input.astype('float')
-        label = data.iloc[:, -1:]
-        label = np.array(label)
-        label = label.astype('float')
-        return input, label
+        data = self.data[index][1]
+        input = data[:-1]
+        label = data[-1:]
+        return torch.from_numpy(input).float(), torch.from_numpy(label).float()
+
+    def __len__(self):
+        return self.data.shape[0]
 
 
-model = Net().double()
-model.train()
-batch_size = 100
+batch_size = 1000
 lr = 0.1
+length_dataset = 1052636
+iterations = length_dataset // batch_size
 
-custom_data_from_csv = MyDataset('/Users/recep/Documents/NumericoTech/multi-agent-incidents-prediction/data/train_data/280312011.csv', batch_size)
-train_loader = data.DataLoader(dataset=custom_data_from_csv)
+dataset = DatasetH5('data/train_data/280312011.h5')
+dataloader = data.DataLoader(dataset=dataset, batch_size=batch_size)
 
+model = Net()
+model.train()
 optimizer = optim.SGD(model.parameters(), lr=lr)
-criterion = nn.MSELoss()
+criterion = nn.BCEWithLogitsLoss()
 
-lenght_dataset = 1052636
-iterations = lenght_dataset // batch_size
-
-for batch_idx in range(iterations):
-    for _, (input, label) in enumerate(train_loader):
-        optimizer.zero_grad()
-        net_out = model(input)
-        loss = criterion(net_out, label)
-        loss.backward()
-        optimizer.step()
+for batch_idx, (input, label) in enumerate(dataloader):
+    optimizer.zero_grad()
+    net_out = model(input)
+    loss = criterion(net_out, label)
+    loss.backward()
+    optimizer.step()
     print(f'{datetime.datetime.now()} | Iteration: {batch_idx}/'
           f'{iterations} | Loss: {loss}')
